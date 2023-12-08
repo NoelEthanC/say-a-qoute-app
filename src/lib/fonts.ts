7e92bbdc18114c534631762f5ace30ee4caec548
NoelEthanC <noelchiwamba1@gmail.com>
2023-12-08 13:09:37 +0200

    main

---
+++ src/lib/fonts.ts
@@ -0,0 +1,4 @@
+import { Yesteryear, Playfair } from "next/font/google";
+
+export const yesteryear = Yesteryear({ subsets: ["latin"], weight: ["400"] });
+export const playfair = Playfair({ subsets: ["latin"], weight: ["400"] });
